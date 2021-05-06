a4d1df2d2f9f0cbe0592a1c9ae0ef978fc0d5e52
chenlinsong <chenlinsong@shouqiev.com>
2021-05-06 19:05:47 +0800

    动态添加sql

--- mybatis-enhance-example/src/main/java/cus/mybatis/enhance/example/Application.java
+++ mybatis-enhance-example/src/main/java/cus/mybatis/enhance/example/Application.java
@@ -1,10 +1,13 @@
 package cus.mybatis.enhance.example;
 
+import cus.mybatis.enhance.example.entity.ActivityExample;
 import cus.mybatis.enhance.example.mapper.ActivityEnhanceExampleMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @MapperScan(value = "cus.mybatis.enhance.example.mapper")
 @SpringBootApplication()
@@ -13,7 +16,11 @@ public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new SpringApplication(Application.class).run(args);
         ActivityEnhanceExampleMapper activityEnhanceMapper = context.getBean(ActivityEnhanceExampleMapper.class);
-        activityEnhanceMapper.selectByPrimaryKey(1);
-        System.out.println("_____:"+activityEnhanceMapper.get().toString());
+        ActivityExample activityExample = activityEnhanceMapper.selectByPrimaryKey(3);
+        if (activityExample != null){
+            System.out.println("_____:"+activityExample.toString());
+        }
+        List<ActivityExample> activityExamples = activityEnhanceMapper.get();
+        System.out.println("+++++:"+activityExamples.toString());
     }
 }
